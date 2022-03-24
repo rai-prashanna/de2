8a39e5be59a70b515776568f0bb40216b165f3cf
prai93 <prai931024@gmail.com>
2022-03-24 12:19:11 +0000

    added ip of broker

--- producer.py
+++ producer.py
@@ -1,6 +1,6 @@
 import pulsar
 # Create a pulsar client by supplying ip address and port
-client = pulsar.Client('pulsar://localhost:6650')
+client = pulsar.Client('pulsar://192.168.2.232:6650')
 # Create a producer on the topic that consumer can subscribe to
 producer = client.create_producer('DEtopic')
 # Send a message to consumer
