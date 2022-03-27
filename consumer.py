762745a1d35ad6d5b8b1f33a148e6a1807e02ea4
rai-prashanna <prai931024@gmail.com>
2022-03-27 16:54:16 +0200

    last task completed

--- consumer.py
+++ consumer.py
@@ -2,13 +2,19 @@ import pulsar
 # Create a pulsar client by supplying ip address and port
 client = pulsar.Client('pulsar://192.168.2.232:6650')
 # Subscribe to a topic and subscription
-consumer = client.subscribe('DEtopic', subscription_name='DE-sub')
+consumer = client.subscribe('C1-Topic', subscription_name='C1-sub')
 # Display message received from producer
 msg = consumer.receive()
+decoded_msg=msg.data().decode()
+#decoded_msg=("number_of_words",number_of_words)
+index=int(decoded_msg)
 try:
-    print("Received message : '%s'" % msg.data())
-# Acknowledge for receiving the message
-    consumer.acknowledge(msg)
+    while index>0:
+        msg=consumer.receive()
+        index=index-1;
+        print('%s' % msg.data().decode().upper())
+        # Acknowledge for receiving the message
+        consumer.acknowledge(msg)
 except:
     consumer.negative_acknowledge(msg)
 # Destroy pulsar client
--- producer.py
+++ producer.py
@@ -1,9 +1,18 @@
+import string
 import pulsar
+import conversion
 # Create a pulsar client by supplying ip address and port
 client = pulsar.Client('pulsar://192.168.2.232:6650')
 # Create a producer on the topic that consumer can subscribe to
-producer = client.create_producer('DEtopic')
-# Send a message to consumer
-producer.send(('Welcome to Data Engineering Course!').encode('utf-8'))
+producer = client.create_producer('C1-Topic')
+#do split operation by space
+splitted_sentence='Welcome to Data Engineering Course!'.split(" ")
+number_of_words=len(splitted_sentence)
+# Send each word to topic
+producer.send(str(number_of_words).encode('utf-8'))
+
+for word in splitted_sentence:
+    producer.send((word).encode('utf-8'))
+
 # Destroy pulsar client
 client.close()
