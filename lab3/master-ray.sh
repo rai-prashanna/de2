7410233f5573aa2189d7f3d1be01ccfdc299d577
rai-prashanna <prai931024@gmail.com>
2022-04-29 09:52:47 +0200

    assignment3

---
+++ lab3/master-ray.sh
@@ -0,0 +1,22 @@
+
+    1  exit
+    2  sudo apt update
+    3  sudo apt upgrade
+    4  sudo apt update
+    5  sudo apt upgrade -y
+    6  .local/bin/ray start --head --port=6379
+    7  python3 -m pip install -U "ray[default]"
+    8  sudo apt install -y python3-pip
+    9  python3 -m pip install --upgrade pip
+   10  python3 -m pip install -U "ray[default]"
+   11  .local/bin/ray start --head --port=6379
+   12  exit
+   13  python3 -m pip install jupyter
+   14  .local/bin/jupyter notebook
+   15  ray stop
+   16  .local/bin/jupyter notebook
+   17  exit
+   18  .local/bin/jupyter notebook
+   19  clear
+   20  ls
+   21  history 
